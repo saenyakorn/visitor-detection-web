0735f31942a5cad8c6515d34617b37d1a53cba45
Saenyakorn <saneyakorn@gmail.com>
2021-05-31 23:55:41 +0700

    🔧 add api captures and devices endpoints

--- src/pages/api/captures.tsx
+++ src/pages/api/captures.tsx
@@ -1,54 +1,33 @@
 import { getToken } from 'next-auth/jwt'
 import { NextApiRequest, NextApiResponse } from 'next'
-import { httpClient } from '@app/config/http'
 import { CaptureDTO } from '@app/dto/capture'
+import { firestore } from '@app/lib/firebase-admin'
+import { getSession } from 'next-auth/client'
 
 const secret = process.env.NEXT_AUTH_SECRET || ''
 
-const mockCaptures: CaptureDTO[] = [
-  {
-    name: 'Front',
-    imageURL: 'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png',
-    createdDate: '12/10/2021 12:00',
-  },
-  {
-    name: 'Back',
-    imageURL: 'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png',
-    createdDate: '12/10/2021 12:00',
-  },
-  {
-    name: 'Back',
-    imageURL: 'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png',
-    createdDate: '12/10/2021 12:00',
-  },
-  {
-    name: 'Back',
-    imageURL: 'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png',
-    createdDate: '12/10/2021 12:00',
-  },
-  {
-    name: 'Back',
-    imageURL: 'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png',
-    createdDate: '12/10/2021 12:00',
-  },
-  {
-    name: 'Back',
-    imageURL: 'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png',
-    createdDate: '12/10/2021 12:00',
-  },
-  {
-    name: 'Back',
-    imageURL: 'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png',
-    createdDate: '12/10/2021 12:00',
-  },
-]
+const findCaptures = async (user: string) => {
+  const collection = firestore.collection('captures')
+  const snapshot = await collection.where('user', '==', user).get()
+  if (snapshot.empty) return []
+  const captures: Omit<CaptureDTO, 'user'>[] = []
+  snapshot.forEach(doc => {
+    const { user, ...rest } = doc.data() as CaptureDTO
+    captures.push(rest)
+  })
+  return captures
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const token = await getToken({ req, secret })
-    if (token?.accessToken) {
-      //const { data } = httpClient.get('/path/to/get/device')
-      res.status(200).json(mockCaptures)
+    const session = await getSession({ req })
+    if (token?.accessToken && session?.user) {
+      if (req.method === 'GET') {
+        const user = session.user.email
+        const captures = await findCaptures(user as string)
+        res.status(200).json(captures)
+      }
     } else {
       res.status(401).send('UNAUTHORIZED')
     }
--- src/pages/api/devices.tsx
+++ src/pages/api/devices.tsx
@@ -1,27 +1,63 @@
 import { getToken } from 'next-auth/jwt'
 import { NextApiRequest, NextApiResponse } from 'next'
-import { httpClient } from '@app/config/http'
+import { firestore } from '@app/lib/firebase-admin'
 import { DeviceDTO } from '@app/dto/devices'
+import { getSession } from 'next-auth/client'
+import jwt from 'jsonwebtoken'
+import { format } from 'date-fns'
 
 const secret = process.env.NEXT_AUTH_SECRET || ''
+const jwtSecret = process.env.JWT_SECRET || ''
 
-const mockDevices: DeviceDTO[] = [
-  {
-    name: 'Front',
-    lastActive: '1/10/2000 12:30',
-  },
-  {
-    name: 'Back',
-    lastActive: '1/10/2000 12:40',
-  },
-]
+const post = async (req: NextApiRequest) => {
+  const name = req.body.name as string
+  const token = await getToken({ req, secret })
+  const session = await getSession({ req })
+  if (token?.accessToken && session?.user) {
+    const user = session.user.email
+    const snapshot = await firestore.collection('devices').where('user', '==', user).get()
+    let hasUniqueName = true
+    snapshot.forEach(doc => {
+      if ((doc.data() as DeviceDTO).name === name) hasUniqueName = false
+    })
+    if (!hasUniqueName) throw new Error("the name isn't unique")
+    const tk = jwt.sign({ user: user, deviceName: name }, jwtSecret)
+    await firestore.collection('devices').add({
+      user: user,
+      name: name,
+      lastActive: format(new Date(), 'yyyy-MM-dd HH:mm:ss'),
+    })
+    return tk
+  }
+}
+
+const get = async (req: NextApiRequest) => {
+  const allDevice: Omit<DeviceDTO, 'user'>[] = []
+  const token = await getToken({ req, secret })
+  const session = await getSession({ req })
+  if (token?.accessToken && session?.user) {
+    const user = session.user.email
+    const snapshot = await firestore.collection('devices').where('user', '==', user).get()
+    snapshot.forEach(doc => {
+      const { user, ...rest } = doc.data() as DeviceDTO
+      allDevice.push(rest)
+    })
+  }
+  return allDevice
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const token = await getToken({ req, secret })
-    if (token?.accessToken) {
-      //const { data } = httpClient.get('/path/to/get/device')
-      res.status(200).json(mockDevices)
+    const session = await getSession({ req })
+    if (token?.accessToken && session?.user) {
+      if (req.method === 'POST') {
+        const tk = await post(req)
+        res.status(200).json({ token: tk })
+      } else if (req.method === 'GET') {
+        const allDevice = await get(req)
+        res.status(200).json(allDevice)
+      }
     } else {
       res.status(401).send('UNAUTHORIZED')
     }
