32628d54cb1529bc42c660567f5daa71d4e4175a
Saenyakorn <saneyakorn@gmail.com>
2021-05-31 23:55:04 +0700

    🔧 add api publish alarm

---
+++ src/pages/api/publish.tsx
@@ -0,0 +1,13 @@
+import mqttClient from '@app/../server/mqtt'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    if (req.method === 'GET') {
+      mqttClient.publish('@msg/alarm', 'on')
+      res.status(200).send('Publish Success')
+    }
+  } catch (err) {
+    res.status(400).send('Something went wrong')
+  }
+}
